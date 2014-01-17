7cdd608dbf0b858f6d1603fab7ac488abb0c62c8
kappa <inokara@gmail.com>
2014-01-18 08:33:05 +0900

    hostname を facter で取得出来るようにした

--- sensu-client/puppet/site.pp
+++ sensu-client/puppet/site.pp
@@ -5,8 +5,9 @@ node default {
     rabbitmq_password => 'mypass',
     rabbitmq_ssl_private_key => "puppet:///mount_point/sensu/key.pem",
     rabbitmq_ssl_cert_chain => "puppet:///mount_point/sensu/cert.pem",
-    rabbitmq_host => 'xxx.xxx.xxx.xxx',
+    rabbitmq_host => '',
     subscriptions => 'sensu-test',
+    client_name   => "${hostname}"
   }
   package { 'nagios-plugins-basic': ensure => latest }
     sensu::check { "cron":
