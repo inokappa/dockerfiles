2c91b5022d867dfdfe18edf1b5497f987947847c
kappa <kappa@kappabookair.local>
2014-05-26 16:03:17 +0900

    add graphite

---
+++ graphite/graphite/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+  'default': {
+    'NAME': 'graphite',
+    'ENGINE': 'django.db.backends.mysql',
+    'USER': 'graphite',
+    'PASSWORD': 'passw0rd',
+    'HOST': 'localhost',
+    'PORT': '3306',
+  }
+}
