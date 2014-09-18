42c015fcc1d430e462893af880bf20f20e402775
kappa <kappa@kappa-at-clp.local>
2014-09-19 02:35:16 +0900

    run foreground mode for sensu-client

--- sensu-client/start.sh
+++ sensu-client/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 #
-/etc/init.d/sensu-client start
+#/etc/init.d/sensu-client start
+/opt/sensu/embedded/bin/ruby /opt/sensu/bin/sensu-client -b -c /etc/sensu/config.json -d /etc/sensu/conf.d -e /etc/sensu/extensions -p /var/run/sensu/sensu-client.pid -l /var/log/sensu/sensu-client.log -L info
